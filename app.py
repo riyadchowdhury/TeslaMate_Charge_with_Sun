90569c530c4885e660ea6596dd8237f3cdb515c5
rchowdhury <riyad.chowdhury@warnermedia.com>
2023-09-05 14:05:15 -0400

    increasing root logging

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from flask import Flask, render_template, redirect, url_for, request
 
 globals.init()
 db_init.initialize_db()
-LOGLEVEL = os.environ.get('LOGGING_LEVEL', 'INFO').upper()
+LOGLEVEL = os.environ.get('LOGGING_LEVEL', 'WARNING').upper()
 logging.basicConfig(level=LOGLEVEL)
-logging.getLogger('httpx').setLevel(logging.WARNING)
+logging.getLogger('root').setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
 config = db_functions.get_config_from_db()
